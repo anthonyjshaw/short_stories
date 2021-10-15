38389e9b8b13b711a913a199243fc471cd10e674
Anthony Shaw <anthonyshaw@sceaga.com>
2021-10-15 21:17:47 +0100

    Created server with mongoose support

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+const uri = process.env.DB_CONNECTION; 
+
+app.use(cors());
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error)=> console.error(error.message));
+
